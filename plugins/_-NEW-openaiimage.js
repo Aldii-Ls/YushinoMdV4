e8028be72f1e8e18a7694da4d3d4ebb07aa1ed30
Aldi lesmana <lesmanaaldi098@gmail.com>
2023-01-25 00:18:57 +0700

    Update _-NEW-openaiimage.js

--- plugins/_-NEW-openaiimage.js
+++ plugins/_-NEW-openaiimage.js
@@ -1,9 +1,9 @@
 import { Configuration, OpenAIApi } from "openai";
 let handler = async (m, { conn, text }) => {
         
-            if (!text) return reply(`Membuat gambar dari AI.\n\nContoh:\n${prefix}${command} Wooden house on snow mountain`);
+            if (!text) thorw (`Membuat gambar dari AI.\n\nContoh:\n${prefix}${command} Wooden house on snow mountain`);
             const configuration = new Configuration({
-              apiKey: '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'
+              apiKey: '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'
             });
             const openai = new OpenAIApi(configuration);
             const response = await openai.createImage({
